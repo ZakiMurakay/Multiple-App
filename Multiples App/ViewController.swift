e0654b538884afc21b60baa4a2c3bf3eef5038c0
ZakiMurakay <zaki@clickchapter.com>
2016-06-26 17:53:24 +0800

    Initial Commit

---
+++ Multiples App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multiples App
+//
+//  Created by Muhammad Zaki Murakay on 26/6/16.
+//  Copyright © 2016 BLESA LABS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
