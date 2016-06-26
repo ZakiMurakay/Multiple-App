88314ae19127ef4d6bbdc33b710a614cfc1db7e0
ZakiMurakay <zaki@clickchapter.com>
2016-06-26 22:01:34 +0800

    Finish Multiple-App Exercise

--- Multiples App/ViewController.swift
+++ Multiples App/ViewController.swift
@@ -9,17 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Properties
+    var multiple = 0
+    var num1 = 0
+    var num2 = 0
+    var maxValue = 100
+    
+    //Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var inputTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var infoLbl: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    //Actions
+    @IBAction func playBtnPressed(sender: UIButton) {
+        if inputTxtNotEmpty() {
+            switchGame()
+        }
+    }
+    
+    @IBAction func addBtnPressed(sender: UIButton) {
+        if reachMaxValue() {
+            switchMain()
+        } else {
+            multiply()
+        }
+        
+    }
+    
+    //Functions
+    func inputTxtNotEmpty() -> Bool {
+        if inputTxt.text != nil && inputTxt.text != "" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func reachMaxValue() -> Bool {
+        if num2 >= maxValue {
+            return true
+        } else {
+            return false
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func multiply() {
+        num1 = num2
+        num2 = num1 + multiple
+        infoLbl.text = "\(num1) + \(multiple) = \(num2)"
     }
-
-
+    
+    func switchMain(){
+        logoImg.hidden = false
+        inputTxt.hidden = false
+        playBtn.hidden = false
+        
+        infoLbl.hidden = true
+        addBtn.hidden = true
+    }
+    
+    func switchGame() {
+        logoImg.hidden = true
+        inputTxt.hidden = true
+        playBtn.hidden = true
+        
+        infoLbl.hidden = false
+        addBtn.hidden = false
+        infoLbl.text = "Press ADD to add!"
+        resetValue()
+    }
+    
+    func resetValue() {
+        multiple = Int(inputTxt.text!)!
+        num1 = 0
+        num2 = 0
+    }
 }
 
